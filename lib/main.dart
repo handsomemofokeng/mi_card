b0b8027c7744665a66fc3a77cec78649a52f3c84
Handsome Mofokeng <handsomem23@hotmail.com>
2020-05-03 06:51:49 +0200

    Added another font family

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,34 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 75.0,
-                backgroundImage: AssetImage('images/handsome.jpg'),
-              ),
-              Text(
-                'Handsome Mofokeng',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 35.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 75.0,
+                  backgroundImage: AssetImage('images/handsome.jpg'),
                 ),
-              ),
-              Container(
-                width: double.infinity,
-              ),
-            ],
+                Text(
+                  'Handsome Mofokeng',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 35.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'MOBILE DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 25.0,
+                    color: Colors.teal[100],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Divider(),
+              ],
+            ),
           ),
         ),
       ),
